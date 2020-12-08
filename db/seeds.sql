cca2c00f6b04476bc183dc3100c58f3718eafb6c
Dan Cornutt <dacornutt@gmail.com>
2020-12-08 15:14:17 -0800

    Updated model, config, and controller

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,6 @@
 INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
 INSERT INTO burgers (burger_name) VALUES ('Bacon Double Cheeseburger');
 INSERT INTO burgers (burger_name) VALUES ('Bison and Blue Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Chicken Burger');
+INSERT INTO burgers (burger_name) VALUES ('BBQ Burger with Hot Links');

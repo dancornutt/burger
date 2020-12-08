424b0512ec299decf3cce70e5b058b944cc13f67
Dan Cornutt <dacornutt@gmail.com>
2020-12-08 14:45:38 -0800

    Initial Commit

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Double Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Bison and Blue Cheese Burger');
